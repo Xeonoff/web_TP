8018d9c73b035f165d3de490f1e1a0a26e4a89e1
Xeonoff <72578165+Xeonoff@users.noreply.github.com>
2024-12-22 22:19:10 +0300

    перенос рутов в приложение

---
+++ web_tp_dz/web_dz_123/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from web_dz_123 import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('question/<int:pk>/', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('tag/<str:name>/', views.tag, name='tag'),
+    path('hot/', views.hot, name='hot'),
+    path('settings/', views.settings, name='profile_editting'),
+    path('member/<str:member_name>/', views.member, name='member')
+]
